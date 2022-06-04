9bc8ff02e3c75cd237d0255e3797abf005999fd7
Namdb <phamtuannam11@gmail.com>
2022-06-04 15:22:10 +0700

    first commit

---
+++ bien.js
@@ -0,0 +1,13 @@
+const a = 10;
+const name = "Nam";
+const isStatus = true;
+const hasChild = true;
+const running = function (){
+    console.log("running");
+}
+running();
+
+const productlist = [
+    { id 1, name: "nam"}
+    { id 2, name: "tuan"}
+];
